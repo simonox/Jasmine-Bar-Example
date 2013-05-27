5ee64e8b769aa6a65d0fe32555c0155c32abb486
Oliver Ochs <simonox@gmail.com>
2013-05-27 21:05:40 +0200

    fixed package and intendation

--- spec/barSpec.js
+++ spec/barSpec.js
@@ -1,12 +1,11 @@
 describe("In order to get a drink as a guest I want a barkeeper to mix it.", function() {
 	describe("An Order", function() {
-	  it("should contain an added Vodka Lemon", function() {
-	    var order = new Order();
-	    order.add(menu.wodkaLemon);
-	    expect(order.items).toContain(menu.wodkaLemon);
-	  });
+		it("should contain an added Vodka Lemon", function() {
+			var order = new Order();
+			order.add(menu.wodkaLemon);
+			expect(order.items).toContain(menu.wodkaLemon);
+		});
 	});
-
 	describe("The barkeeper", function(){
 		it("should return an ordered wodka lemon", function() {
 			var order = new Order();
