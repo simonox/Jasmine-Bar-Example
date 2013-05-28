74af0a632d6364d6583088b41aff4a89d4048990
Oliver Ochs <simonox@gmail.com>
2013-05-28 10:28:06 +0200

    Vodka always with W (Wodka)

--- spec/barSpec.js
+++ spec/barSpec.js
@@ -1,13 +1,13 @@
 describe("In order to get a drink as a guest I want a barkeeper to mix it.", function() {
-	describe("An Order", function() {
-		it("should contain an added Vodka Lemon", function() {
+	describe("An order", function() {
+		it("should contain an added Wodka Lemon", function() {
 			var order = new Order();
 			order.add(menu.wodkaLemon);
 			expect(order.items).toContain(menu.wodkaLemon);
 		});
 	});
 	describe("The barkeeper", function(){
-		it("should return an ordered wodka lemon", function() {
+		it("should return an ordered Wodka Lemon", function() {
 			var order = new Order();
 			order.add(menu.wodkaLemon);
 			// oops, nur ein Mock
